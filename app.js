b9d44d5fd95ef8b6f8f6919031dc44349635fc51
Ameymin03 <152912235+Ameymin03@users.noreply.github.com>
2024-03-14 23:51:00 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,26 +2,12 @@ const express=require('express');
 const bodyParser=require('body-parser')
 const app=express();
 
+const adminRoutes=require('./routes/admin')
+const shopRoutes=require('./routes/shop')
+
 app.use(bodyParser.urlencoded({extended: false}));
-app.use('/',(req,res,next)=>{
-    console.log('This always runs')
-    next();
-})
 
-app.use('/add-products',(req,res,next)=>{
-    console.log('Hey there! ')
-    res.send('<form action="/products" method="POST">Product name<input type="text" name="title">Procuct Size<input type="number" name="title"><button type="submit">Add product</button></form>')
-})
-app.use('/products',(req,res,next)=>{
-    console.log(req.body)
-    
-    res.redirect('/')
-})
-
-
-app.use('/',(req,res,next)=>{
-    console.log('In the Middleware! ')
-    res.send('<h1>Record Submitted successfully</h1>')
-})
+app.use(adminRoutes);
+app.use(shopRoutes)
 
 app.listen(8080);
